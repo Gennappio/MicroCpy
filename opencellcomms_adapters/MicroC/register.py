93233dc12a8151138d6ce42179e724b51650d1b0
Gennaro Abbruzzese <gennaro@endymion.tech>
2026-06-15 12:19:46 +0200

    Update MicroC plugin workflow metadata

--- opencellcomms_adapters/MicroC/register.py
+++ opencellcomms_adapters/MicroC/register.py
@@ -13,12 +13,12 @@ function name is a benign overwrite (same code, same signature).
 import opencellcomms_adapters.MicroC.functions.gene_network.initialize_netlogo_gene_networks  # noqa: F401
 import opencellcomms_adapters.MicroC.functions.gene_network.propagate_gene_networks_netlogo  # noqa: F401
 
-# Intercellular fate functions
-import opencellcomms_adapters.MicroC.functions.intercellular.mark_necrotic_cells  # noqa: F401
-import opencellcomms_adapters.MicroC.functions.intercellular.mark_apoptotic_cells  # noqa: F401
-import opencellcomms_adapters.MicroC.functions.intercellular.mark_growth_arrest_cells  # noqa: F401
-import opencellcomms_adapters.MicroC.functions.intercellular.mark_proliferating_cells  # noqa: F401
+# Fate functions
+import opencellcomms_adapters.MicroC.functions.fate.mark_necrotic_cells  # noqa: F401
+import opencellcomms_adapters.MicroC.functions.fate.mark_apoptotic_cells  # noqa: F401
+import opencellcomms_adapters.MicroC.functions.fate.mark_growth_arrest_cells  # noqa: F401
+import opencellcomms_adapters.MicroC.functions.fate.mark_proliferating_cells  # noqa: F401
 
-# Finalization plots
-import opencellcomms_adapters.MicroC.functions.finalization.generate_iteration_plots  # noqa: F401
-import opencellcomms_adapters.MicroC.functions.finalization.generate_summary_plots  # noqa: F401
+# Reporting plots
+import opencellcomms_adapters.MicroC.functions.reporting.generate_iteration_plots  # noqa: F401
+import opencellcomms_adapters.MicroC.functions.reporting.generate_summary_plots  # noqa: F401
